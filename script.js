f8ef8b82606be8aa442d73439d3d0d88b7ac022c
del-ereno <delereno2@gmail.com>
2025-03-05 17:06:25 +1300

    linked files to index, added operator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+//add function
+function add(a,b){
+    return a + b;
+}
+
+//subtract function
+function subtract(a,b){
+    return a - b;
+}
+
+//multiply function
+function multiply(a,b){
+    return a * b;
+}
+
+//divide function
+function divide(a,b){
+    return a / b;
+}
+
