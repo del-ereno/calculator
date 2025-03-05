72e301298e28fbf98f9410c4c2f02f6aac72a2bd
del-ereno <delereno2@gmail.com>
2025-03-05 17:11:22 +1300

    operate function created to facilitate running calcs

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function divide(a,b){
     return a / b;
 }
 
+var num1;
+var num2;
+var operator;
+
+function operate(op, n1, n2){
+    if (op === "+"){
+        return add(n1,n2);
+    }
+    else if (op === "-"){
+        return subtract(n1,n2); 
+    }
+    else if (op === "*"){
+        return multiply(n1,n2); 
+    }
+    else if (op === "/"){
+        return divide(n1,n2); 
+    }
+}
